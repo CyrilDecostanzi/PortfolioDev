5610cde4551ed8fe3fc8223e09e6db44ee83f74c
Cyril <decostanzicyril@gmail.com>
2021-01-09 23:41:22 +0100

    fix bug

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ function init() {
   container = document.querySelector(".scene");
 
   //Create scene
-   
+
   scene = new THREE.Scene();
 
   const fov = 60;
@@ -21,20 +21,18 @@ function init() {
   const far = 1000;
 
   //Camera setup
-   
+
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(0, 4, 25);
 
-   
   const ambient = new THREE.AmbientLight(0x000025, 2);
   scene.add(ambient);
 
-   
   const light = new THREE.DirectionalLight(0x000136, 1);
   light.position.set(50, 50, 100);
   scene.add(light);
   //Renderer
-   
+
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setSize(container.clientWidth, container.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
@@ -42,9 +40,9 @@ function init() {
   container.appendChild(renderer.domElement);
 
   //Load Model
-   
+
   let loader = new THREE.GLTFLoader();
-   
+
   loader.load("../house/scene.gltf", function (gltf) {
     scene.add(gltf.scene);
     house = gltf.scene.children[0];
@@ -54,7 +52,7 @@ function init() {
 
 function fastAnimate() {
   requestAnimationFrame(fastAnimate);
-  house.rotation.z += 0.010;
+  house.rotation.z += 0.005;
   renderer.render(scene, camera);
 }
 
